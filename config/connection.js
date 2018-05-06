2e8ed5be8a776f5ad85f387ec12a7ead51886528
Frank Dominguez <frankd1997@gmail.com>
2018-05-06 16:10:15 -0500

    gorp

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+//Require mysql npm packages
+var mysql = require("mysql");
+//Create connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+//Handle errors and log the MYSQL connection ID to the console
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//Export the connection.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Bacon Ranch Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", 0);
